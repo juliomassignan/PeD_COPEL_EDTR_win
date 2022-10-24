23df17f111fbc1f017aabad8f3bf3acdc746a8f5
Vitor <vitorhpmelo@gmail.com>
2022-10-24 20:46:46 -0300

    modificacoes no main

--- main.c
+++ main.c
@@ -242,11 +242,6 @@ int main(int argc, char** argv) {
     // funcao principla para realizacao do nowcasting de demanda
     resultadoNC=NowCastingDemanda(grafo_tf,barra_tf,numeroBarras_tf,alimentador_tf,curvasTrafos,numeroAlimentadores,ramo_tf,Sbase,interfaceNiveis_tf,numeroInterfaces_tf,areasMedicao_tf,prev_tf,medidaPrev_tf,numeroMedidas,numeroAmostras);
     
-
-
-
-
-    
     //    salva (curva de carga de demanda estimada, resultados condensados do fluxo de potencia )
     // resultados de saída
     // Curvas Agregadas e fazer igual
